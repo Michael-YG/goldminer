2d316855106ad48953537157d0487f5a8965b70b
ms10 <michaelsongxx@gmail.com>
2023-03-24 17:49:56 -0400

    udpate

--- hw/rtl/sha256_module.sv
+++ hw/rtl/sha256_module.sv
@@ -1,6 +1,7 @@
 `timescale 1ns/1ps
 
 `include "sha256_incl.svh"
+`define OPTIMIZE
 
 module sha256_module (
     input clk, reset, start,
@@ -71,7 +72,7 @@ always_ff @ (posedge clk) begin
         end
 end
 
-assign data_0 = data_in[(cnt_1 << 3) << 24];
+assign data_0 = data_in[(cnt_1 << 3)] << 24;
 assign data_1 = {{8{1'b0}},(data_in[((cnt_1 + 1) << 3)]<<16)};
 assign data_2 = {{16{1'b0}},(data_in[(cnt_1 + 2) << 3] << 8)};
 assign data_3 = {{24{1'b0}},data_in[(cnt_1 + 3) << 3]};
@@ -81,7 +82,10 @@ always_ff @ (posedge clk) begin
         for (int i = 0; i < 63; i=i+1)
             mem_m[i] <= 0;
     else begin
-        mem_m <= data_0 | data_1 | data_2 | data_3;
+        if(cnt_0[4] == 0 && cnt_2[4] == 0)
+            mem_m[cnt_0] <= data_0 | data_1 | data_2 | data_3;
+        else
+            mem_m[cnt_0] <= sig1(mem_m[cnt_0-2]) + mem_m[cnt_0-7] + sig0(mem_m[cnt_0-15]) + mem_m[cnt_0-16];
     end
 end
 
@@ -93,18 +97,55 @@ always_ff @ (posedge clk) begin
         else cnt_2 <= cnt_2 == 0? 0 : cnt_2 + 1;
 end
 
+logic [31:0] t1,t2;
+assign t1 = h + sig1(e) + ch(e,f,g) + mem_k[cnt_2] + mem_m[cnt_2];
+assign t2 = sig0(a) + maj(a,b,c);
+
+always_ff @ (posedge clk) begin
+    if(cnt_0 == 0) begin
+        a <= data_out[31:0];
+        b <= data_out[63:32];
+        c <= data_out[95:64];
+        d <= data_out[127:96]; 
+        e <= data_out[159:128];
+        f <= data_out[191:160];
+        g <= data_out[223:192];
+        h <= data_out[255:224];
+    end else begin
+        if(cnt_0 != 0) begin
+            h <= g;
+            g <= f;
+            f <= e;
+            e <= d + t1;
+            d <= c;
+            c <= b;
+            b <= a;
+            a <= t1 + t2;
+        end
+    end
+end
+
 always_ff @ (posedge clk) begin
     if(reset) begin
-        a <= `SHA256_H0;
-        b <= `SHA256_H1;
-        c <= `SHA256_H2;
-        d <= `SHA256_H3;
-        e <= `SHA256_H4;
-        f <= `SHA256_H5;
-        g <= `SHA256_H6;
-        h <= `SHA256_H7;
+        data_out[31:0] <= `SHA256_H0;
+        data_out[63:32] <= `SHA256_H1;
+        data_out[95:64] <= `SHA256_H2;
+        data_out[127:96] <= `SHA256_H3;
+        data_out[159:128] <= `SHA256_H4;
+        data_out[191:160] <= `SHA256_H5;
+        data_out[223:192] <= `SHA256_H6;
+        data_out[255:224] <= `SHA256_H7;
     end else begin
-
+        if(done) begin
+            data_out[31:0] <= a+data_out[31:0];
+            data_out[63:32] <= b+data_out[63:32];
+            data_out[95:64] <= c+data_out[95:64];
+            data_out[127:96] <= d+data_out[127:96];
+            data_out[159:128] <= e+data_out[159:128];
+            data_out[191:160] <= f+data_out[191:160];
+            data_out[223:192] <= g+data_out[223:192];
+            data_out[255:224] <= h+data_out[255:224];
+        end
     end
 end
 
