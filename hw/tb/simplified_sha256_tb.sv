2b1b1d689fcb8b028f4370449a225091690b732a
zm2404 <126287478+zm2404@users.noreply.github.com>
2023-03-29 13:40:56 -0400

    Update simplified_sha256_tb.sv

--- hw/tb/simplified_sha256_tb.sv
+++ hw/tb/simplified_sha256_tb.sv
@@ -28,7 +28,7 @@ module simple_sha256_tb();
 		test_input = {8'b00110001,8'b00110010,8'b00110011,8'b00110100,8'b00110101,8'b00110110,8'b00110111,8'b00111000,
 				8'b00111001,8'b00110000,8'b10000000,416'b0,8'b01010000};	// 512 bits
 		sha_input = test_input;		
-		test_output = 64'hc775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646;	// 256 bits
+		test_output = 256'hc775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646;	// 256 bits
 
 		@(posedge clk);
 		go = 1;
@@ -69,7 +69,7 @@ module simple_sha256_tb();
 			8'b00000000,8'b00000000,8'b00000000,8'b00000000,
 			8'b00000000,8'b00000000,8'b00000001,8'b10011000};
 		sha_input = test_input;		
-		test_output = 64'h5165b7f3f2e2a96f3a87bc6800a4e051618b1e12d78c5c7ef3f86cd3a04f654e;
+		test_output = 256'h5165b7f3f2e2a96f3a87bc6800a4e051618b1e12d78c5c7ef3f86cd3a04f654e;
 
 		@(posedge clk);
 		go = 1;
@@ -112,7 +112,7 @@ module simple_sha256_tb();
 			,8'b00000000,8'b00000000,8'b00000000,8'b00000000
 			,8'b00000000,8'b00000000,8'b00000001,8'b00110000};
 		sha_input = test_input;		
-		test_output = 64'h386d4cd64fd1d2e053498544f87ced9bac56ed2988ac58d70e50bbacd374ac21;
+		test_output = 256'h386d4cd64fd1d2e053498544f87ced9bac56ed2988ac58d70e50bbacd374ac21;
 
 		@(posedge clk);
 		go = 1;
@@ -155,7 +155,7 @@ module simple_sha256_tb();
 			,8'b00000000,8'b00000000,8'b00000000,8'b00000000
 			,8'b00000000,8'b00000000,8'b00000001,8'b10110000};
 		sha_input = test_input;		
-		test_output = 64'h582ebf39bdb7505eb4da7b39c343d9b780544b1e46a238000bb1b1758a3b5731;
+		test_output = 256'h582ebf39bdb7505eb4da7b39c343d9b780544b1e46a238000bb1b1758a3b5731;
 
 		@(posedge clk);
 		go = 1;
