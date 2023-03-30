add4bf249bee9cff28624b2d84ce7be4642f0253
Michael-YGm <michaelsongxx@gmail.com>
2023-03-30 00:24:06 -0400

    update

--- hw/rtl/sha256_module.sv
+++ hw/rtl/sha256_module.sv
@@ -31,7 +31,7 @@ endmodule
 //endfunction
 
 module maj (input [31:0] x,y,z, output [31:0] maj);
-    assign maj = (z & y) ^ (x & z) ^ (y & z);
+    assign maj = (x & y) ^ (x & z) ^ (y & z);
 endmodule
 //// epsilone0 function
 //function logic [31:0] ep0(input [31:0] x)
@@ -54,14 +54,14 @@ endmodule
 //    sig0 = {{7{1'b0}},x[31:7]} ^ {{18{1'b0}},x[31:18]} ^ {{3{1'b0}},x[31:3]};
 //endfunction
 module sig0(input [31:0] x, output [31:0] sig0);
-    assign sig0 = rotR(x,7) ^ rotR(x,18) ^ ({3{1'b0},x[31:3]});
+    assign sig0 = rotR(x,7) ^ rotR(x,18) ^ ({{3{1'b0}},x[31:3]});
 endmodule
 //function logic [31:0] sig1(input [31:0] x)
 //    // sigma1 = (x >> 17) ^ (x >> 19) ^ (x >> 10);
 //    sig1 = {{17{1'b0}},x[31:17]} ^ {{19{1'b0}},x[31:19]} ^ {{10{1'b0}},x[31:10]};
 //endfunction
 module sig1(input [31:0] x, output [31:0] sig1);
-    assign sig1 = rotR(x,17) ^ rotR(x,19) ^ ({10{1'b0},x[31:10]});
+    assign sig1 = rotR(x,17) ^ rotR(x,19) ^ ({{10{1'b0}},x[31:10]});
 endmodule
 
 // enum int unsigned { S0 = , S1 = 1} state, next_state;
@@ -89,33 +89,19 @@ logic [31:0] data_0,data_1,data_2,data_3;
 always_ff @ (posedge clk) begin
     if(reset) begin
         cnt_0 <= 0;
-        // cnt_1 <= 0;
     end
     else
         if(start) begin
             cnt_0 <= cnt_0 + 1;
-            // cnt_1 <= cnt_1 + 4;
         end
         else begin 
-            cnt_0 <= cnt_0 == 0? 0 : cnt_0 + 1;
-            // cnt_1 <= cnt_1 == 0? 0 : cnt_1 + 4;
+            cnt_0 <= cnt_0 == 0? 0 :( cnt_0 == 63? cnt_0:cnt_0 + 1);
         end
 end
 
-// logic [7:0] data_0_select, data_1_select, data_2_select, data_3_select;
-// assign data_0_select = data_in[(cnt_1 << 3) +: 8];
-// assign data_1_select = data_in[((cnt_1 + 1) << 3) +: 8];
-// assign data_2_select = data_in[(cnt_1 + 2) << 3 +: 8];
-// assign data_3_select = data_in[(cnt_1 + 3) << 3 +: 8];
-
-// assign data_0 = data_0_select << 24;
-// assign data_1 = {{8{1'b0}},(data_1_select << 16)};
-// assign data_2 = {{16{1'b0}},(data_2_select << 8)};
-// assign data_3 = {{24{1'b0}},data_3_select};
-
 logic [31:0] sig1_next_0;
 logic [31:0] sig0_next_0;
-logic [9:0] address;
+logic [11:0] address;
 
 assign address = cnt_0 * 32;
 
@@ -127,11 +113,9 @@ always_ff @ (posedge clk) begin
         for (int i = 0; i < 64; i=i+1)
             mem_m[i] <= 0;
     else begin
-        if(cnt_0[4] == 0 && cnt_2[4] == 0)
-            // mem_m[cnt_0] <= data_0 | data_1 | data_2 | data_3;
+        if(cnt_0[4] == 0 && cnt_2[4] == 0 && !cnt_0[5])
             mem_m[cnt_0] <= data_in[address +: 32];
         else
-            // mem_m[cnt_0] <= sig1(mem_m[cnt_0-2]) + mem_m[cnt_0-7] + sig0(mem_m[cnt_0-15]) + mem_m[cnt_0-16];
             mem_m[cnt_0] <= sig1_next_0 + mem_m[cnt_0-7] + sig0_next_0 + mem_m[cnt_0-16];
     end
 end
@@ -140,33 +124,38 @@ end
 always_ff @ (posedge clk) begin
     if (reset) cnt_2 <= 0;
     else
-        if(cnt_0==1) cnt_2 <= cnt_2+1;
-        else cnt_2 <= cnt_2 == 0? 0 : cnt_2 + 1;
+        if(start) cnt_2 <= 0;
+        else 
+            if (cnt_0==1) cnt_2 <= cnt_2+1;
+            else cnt_2 <= cnt_2 == 0? 0 : (cnt_2 == 63? cnt_2:cnt_2 + 1);
 end
 
-logic [31:0] sig0_next_1,sig1_next_1,ch_next_0,maj_next_0;
+logic [31:0] ep0_next,ep1_next,ch_next_0,maj_next_0;
 logic [31:0] t1,t2;
-sig0 sig0_1(.x(a),.sig0(sig0_next_1));
-sig1 sig1_1(.x(e),.sig1(sig1_next_1));
+ep0 ep0_0(.x(a),.ep0(ep0_next));
+ep1 ep1_0(.x(e),.ep1(ep1_next));
 ch ch_0(.x(e),.y(f),.z(g),.ch(ch_next_0));
 maj maj_0(.x(a),.y(b),.z(c),.maj(maj_next_0));
 // assign t1 = h + sig1(e) + ch(e,f,g) + mem_k[cnt_2] + mem_m[cnt_2];
 // assign t2 = sig0(a) + maj(a,b,c);
-assign t1 = h + sig1_next_1 + ch_next_0 + mem_k[cnt_2] + mem_m[cnt_2];
-assign t2 = sig0_next_1 + maj_next_0;
+assign t1 = h + ep1_next + ch_next_0 + mem_k[cnt_2] + mem_m[cnt_2];
+assign t2 = ep0_next + maj_next_0;
+
+logic cnt_0is0;
+assign cnt_0is0 = cnt_0 == 0;
 
 always_ff @ (posedge clk) begin
-    if(cnt_0 == 0) begin
-        a <= data_out[31:0];
-        b <= data_out[63:32];
-        c <= data_out[95:64];
-        d <= data_out[127:96]; 
-        e <= data_out[159:128];
-        f <= data_out[191:160];
-        g <= data_out[223:192];
-        h <= data_out[255:224];
+    if(cnt_0is0) begin
+        h <= data_out[31:0];
+        g <= data_out[63:32];
+        f <= data_out[95:64];
+        e <= data_out[127:96]; 
+        d <= data_out[159:128];
+        c <= data_out[191:160];
+        b <= data_out[223:192];
+        a <= data_out[255:224];
     end else begin
-        if(cnt_0 != 0) begin
+        if(!cnt_0is0 && !done) begin
             h <= g;
             g <= f;
             f <= e;
@@ -181,28 +170,44 @@ end
 
 always_ff @ (posedge clk) begin
     if(reset) begin
-        data_out[31:0] <= `SHA256_H0;
-        data_out[63:32] <= `SHA256_H1;
-        data_out[95:64] <= `SHA256_H2;
-        data_out[127:96] <= `SHA256_H3;
-        data_out[159:128] <= `SHA256_H4;
-        data_out[191:160] <= `SHA256_H5;
-        data_out[223:192] <= `SHA256_H6;
-        data_out[255:224] <= `SHA256_H7;
+        data_out[255:224] <= `SHA256_H0;
+        data_out[223:192] <= `SHA256_H1;
+        data_out[191:160] <= `SHA256_H2;
+        data_out[159:128] <= `SHA256_H3;
+        data_out[127:96] <= `SHA256_H4;
+        data_out[95:64] <= `SHA256_H5;
+        data_out[63:32] <= `SHA256_H6;
+        data_out[31:0] <= `SHA256_H7;
     end else begin
         if(done) begin
-            data_out[31:0] <= a+data_out[31:0];
-            data_out[63:32] <= b+data_out[63:32];
-            data_out[95:64] <= c+data_out[95:64];
-            data_out[127:96] <= d+data_out[127:96];
-            data_out[159:128] <= e+data_out[159:128];
-            data_out[191:160] <= f+data_out[191:160];
-            data_out[223:192] <= g+data_out[223:192];
-            data_out[255:224] <= h+data_out[255:224];
+//            data_out[31:0] <= h+data_out[31:0];
+//            data_out[63:32] <= g+data_out[63:32];
+//            data_out[95:64] <= f+data_out[95:64];
+//            data_out[127:96] <= e+data_out[127:96];
+//            data_out[159:128] <= d+data_out[159:128];
+//            data_out[191:160] <= c+data_out[191:160];
+//            data_out[223:192] <= b+data_out[223:192];
+//            data_out[255:224] <= a+data_out[255:224];
+            data_out[255:224] <= a+`SHA256_H0;
+            data_out[223:192] <= b+`SHA256_H1;
+            data_out[191:160] <= c+`SHA256_H2;
+            data_out[159:128] <= d+`SHA256_H3;
+            data_out[127:96] <= e+`SHA256_H4;
+            data_out[95:64] <= f+`SHA256_H5;
+            data_out[63:32] <= g+`SHA256_H6;
+            data_out[31:0] <= h+`SHA256_H7;
         end
     end
 end
 
-assign done = cnt_2 == 63;
-
+//logic done_next;
+always_ff@(posedge clk)
+    if(reset) begin 
+//        done_next <= 0;
+        done <= 0;
+    end else begin
+//        done_next <= cnt_2 == 63;
+        if(start) done <= 0;
+        else done <= cnt_2 == 63;
+    end
 endmodule
