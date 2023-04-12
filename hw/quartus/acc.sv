f51b0f2167255445b3e5d14dc74f15a9f07f534a
ms10 <michaelsongxx@gmail.com>
2023-04-12 16:29:07 -0400

    u

--- hw/quartus/acc.sv
+++ hw/quartus/acc.sv
@@ -86,14 +86,14 @@ always_ff @ (posedge clk)
                 // 5: data_out <= hashvalue[191:160];
                 // 6: data_out <= hashvalue[223:192];
                 // 7: data_out <= hashvalue[255:224];
-                0: data_out <= hashvalue[287:256];
-                1: data_out <= hashvalue[319:288];
-                2: data_out <= hashvalue[351:320];
-                3: data_out <= hashvalue[383:352];  
-                4: data_out <= hashvalue[415:384];
-                5: data_out <= hashvalue[447:416];
-                6: data_out <= hashvalue[479:448];
-                7: data_out <= hashvalue[511:480];
+                0: data_out <= buffer[287:256];
+                1: data_out <= buffer[319:288];
+                2: data_out <= buffer[351:320];
+                3: data_out <= buffer[383:352];  
+                4: data_out <= buffer[415:384];
+                5: data_out <= buffer[447:416];
+                6: data_out <= buffer[479:448];
+                7: data_out <= buffer[511:480];
                 17: data_out <= 32'h11111111;
                 default: data_out <= 2;
             endcase
