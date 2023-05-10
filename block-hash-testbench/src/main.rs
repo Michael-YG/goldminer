bec45bf2bc1ae347e168fb4875bba2ce549cbc4b
Jules Comte <jgc2158@columbia.edu>
2023-05-10 02:36:35 +0000

    conclude timers

--- block-hash-testbench/src/main.rs
+++ block-hash-testbench/src/main.rs
@@ -7,10 +7,13 @@ use std::time::{Duration, Instant};
 
 mod acc;
 mod sha256_hw;
+mod sha256_sw;
 
 const DEBUG: bool = true;
+const TESTS: u32 = 25;
 
-fn run_test(height: u32) -> Result<(), Box<dyn std::error::Error>> {
+fn run_test(height: u32, gold_time: &mut Duration, hw_time: &mut Duration, sw_time: &mut Duration) -> Result<(), Box<dyn std::error::Error>> {
+    let mut hw = true;
     // GET HEADER BYTES
     let header_bytes = get_block_header(height)?;
 
@@ -21,6 +24,7 @@ fn run_test(height: u32) -> Result<(), Box<dyn std::error::Error>> {
     let gold_hash = header.block_hash().to_raw_hash();
     let end = Instant::now();
     let duration = end - start;
+    *gold_time = *gold_time + duration;
 
     if DEBUG {
         println!(" BLOCK  {}", height);
@@ -28,13 +32,35 @@ fn run_test(height: u32) -> Result<(), Box<dyn std::error::Error>> {
         println!("        {:?}", duration);
     }
 
-    for i in 0..3 {
-        print!(" HW[{}]  ", i);
+    for i in 0..4 {
+        if i == 3 {
+            hw = false;
+        }
+        if hw {
+            print!(" HW[{}]  ", i);
+        } else {
+            print!(" SW     ");
+        }
+
         // GET HARDWARE HASH
-        let start = Instant::now();
-        let hash = sha256_hw::get_hash(&sha256_hw::get_hash(&header_bytes, i), i);
-        let end = Instant::now();
-        let duration = end - start;
+        let start: Instant;
+        let end: Instant;
+        let duration: Duration;
+        let hash: Vec<u8>;
+
+        if hw {
+            start = Instant::now();
+            hash = sha256_hw::get_hash(&sha256_hw::get_hash(&header_bytes, i), i);
+            end = Instant::now();
+            duration = end - start;
+            *hw_time = *hw_time + duration;
+        } else {
+            start = Instant::now();
+            hash = sha256_sw::get_hash(&sha256_sw::get_hash(&header_bytes));
+            end = Instant::now();
+            duration = end - start;
+            *sw_time = *sw_time + duration;
+        };
 
         if DEBUG {
             print!("{}  ", hex::encode(&hash));
@@ -81,9 +107,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let tip_height = get_tip_height()?;
     let mut rng = thread_rng();
     let range = 0..tip_height;
-    for _ in 0..5 {
+    let mut gold_time: Duration = Duration::ZERO;
+    let mut hw_time: Duration = Duration::ZERO;
+    let mut sw_time: Duration = Duration::ZERO;
+    for _ in 0..TESTS {
         let height = rng.gen_range(range.clone());
-        run_test(height)?;
+        run_test(height, &mut gold_time, &mut hw_time, &mut sw_time)?;
     }
+
+    let average_gold = gold_time / TESTS;
+    let average_hw = hw_time / (TESTS * 3);
+    let average_sw = sw_time / TESTS;
+    println!("Average gold time: {:?}", average_gold);
+    println!("Average hw time  : {:?}", average_hw);
+    println!("Average sw time  : {:?}", average_sw);
+
     Ok(())
 }
